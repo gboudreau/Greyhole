73fdfc1f80c4517f47937ca305f7d88d2d4ba09e
gboudreau <guillaume@greyhole.net>
2010-02-16 19:28:25 +0000

    Bugfix: decreasing the number of copies to keep on an existing share could corrupt modified files: the deprecated copy could be kept in favor of the up to date copy...
    git-svn-id: http://greyhole.googlecode.com/svn/trunk@40 b36e06b6-ea79-11de-95aa-47033305a456

--- greyhole.cron.d
+++ greyhole.cron.d
@@ -1,6 +1,9 @@
 # Daily fsck
 0 0 * * *	root	/usr/bin/greyhole-executer --fsck --email-report > /dev/null
 
+# Daily restart; clears caches etc. to keep memory usage in check
+0 0 * * *	root	service greyhole restart
+
 # cifs client workaround
 # Ref: http://blog.dhampir.no/content/cifs-vfs-no-response-for-cmd-n-mid
 @reboot		root	/sbin/modprobe cifs && echo 0 > /proc/fs/cifs/OplockEnabled
