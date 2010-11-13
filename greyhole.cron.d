c4816bf4650ac28855f0db7b6e82c8967b82343e
gboudreau <guillaume@greyhole.net>
2010-11-13 19:09:31 +0000

    Improvement: only 'walk' the graveyard once a week, instead of daily. Will save a lot of time for people with a lot of directories.
    git-svn-id: http://greyhole.googlecode.com/svn/trunk@234 b36e06b6-ea79-11de-95aa-47033305a456

--- greyhole.cron.d
+++ greyhole.cron.d
@@ -1,5 +1,8 @@
 # Daily fsck
-0 0 * * *	root	/usr/bin/greyhole --fsck --email-report > /dev/null
+0 0 * * 1-6	root	/usr/bin/greyhole --fsck --email-report > /dev/null
+
+# Weekly fsck
+0 0 * * 7	root	/usr/bin/greyhole --fsck --email-report --walk-graveyard > /dev/null
 
 # Daily restart; clears caches etc. to keep memory usage in check
 59 23 * * *	root	/sbin/service greyhole condrestart > /dev/null
