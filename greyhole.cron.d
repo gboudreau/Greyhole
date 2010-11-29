8a626895f6c8f36ab3c7e6b5eabd7ba53524fecf
gboudreau <guillaume@greyhole.net>
2010-11-29 14:26:14 +0000

    Improvement: removing daily daemon restart; not needed anymore
    git-svn-id: http://greyhole.googlecode.com/svn/trunk@269 b36e06b6-ea79-11de-95aa-47033305a456

--- greyhole.cron.d
+++ greyhole.cron.d
@@ -4,9 +4,6 @@
 # Weekly fsck
 0 0 * * 7	root	/usr/bin/greyhole --fsck --email-report > /dev/null
 
-# Daily restart; clears caches etc. to keep memory usage in check
-59 23 * * *	root	/sbin/service greyhole condrestart > /dev/null
-
 # cifs client workaround
 # Ref: http://blog.dhampir.no/content/cifs-vfs-no-response-for-cmd-n-mid
 @reboot		root	/sbin/modprobe cifs && echo 0 > /proc/fs/cifs/OplockEnabled
