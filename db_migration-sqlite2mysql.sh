2039fd586525865f310d41b64fe4327e0011c345
gboudreau <guillaume@greyhole.net>
2010-11-30 17:07:20 +0000

    Bugfix: SQLite to MySQL migration (Amahi) bugfixes
    git-svn-id: http://greyhole.googlecode.com/svn/trunk@280 b36e06b6-ea79-11de-95aa-47033305a456

--- db_migration-sqlite2mysql.sh
+++ db_migration-sqlite2mysql.sh
@@ -12,8 +12,8 @@ echo "Done."
 echo
 
 echo "Creating MySQL database and user..."
-hda-create-db-and-user greyhole3
-mysql -u greyhole3 -pgreyhole3 greyhole3 < /usr/share/greyhole/schema-mysql.sql
+hda-create-db-and-user greyhole
+mysql -u greyhole -pgreyhole greyhole < /usr/share/greyhole/schema-mysql.sql
 echo "Done."
 echo
 
@@ -27,6 +27,12 @@ sed -ie 's/^.*db_name: .*/db_name: greyhole/' /var/hda/platform/html/config/grey
 sed -ie 's/^.*db_user: .*/db_user: greyhole/' /var/hda/platform/html/config/greyhole.yml
 sed -ie 's/^.*db_pass: .*/db_pass: greyhole/' /var/hda/platform/html/config/greyhole.yml
 sed -ie 's/^.*db_host: .*/db_host: localhost/' /var/hda/platform/html/config/greyhole.yml
+cat >> /etc/greyhole.conf<<EOF
+db_host = localhost
+db_user = greyhole
+db_pass = greyhole
+db_name = greyhole
+EOF
 sed -ie 's/^.*db_engine = .*/db_engine = mysql/' /etc/greyhole.conf
 sed -ie 's/^.*db_host = .*/db_host = localhost/' /etc/greyhole.conf
 sed -ie 's/^.*db_user = .*/db_user = greyhole/' /etc/greyhole.conf
@@ -39,8 +45,8 @@ echo "Migrating data from SQLite to MySQL..."
 sqlite3 /var/cache/greyhole.sqlite ".dump settings tasks" | grep INSERT | grep -v sqlite_sequence > greyhole.dump.sql
 sed -ie 's/INSERT INTO "tasks"/INSERT INTO tasks/' greyhole.dump.sql
 sed -ie 's/INSERT INTO "settings"/INSERT INTO settings/' greyhole.dump.sql
-mysql -u greyhole3 -pgreyhole3 -e 'truncate settings' greyhole3
-mysql -u greyhole3 -pgreyhole3 greyhole3 < greyhole.dump.sql
+mysql -u greyhole -pgreyhole -e 'truncate settings' greyhole
+mysql -u greyhole -pgreyhole greyhole < greyhole.dump.sql
 rm greyhole.dump.sql
 echo "Done."
 echo
