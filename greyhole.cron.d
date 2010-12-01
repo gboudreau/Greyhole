ba47ea96608088d1c6721f9b033bff64b47fb081
gboudreau <guillaume@greyhole.net>
2010-12-01 16:05:03 +0000

    Improvement: removed daily fsck; only weekly (sunday am) fsck is left now
    git-svn-id: http://greyhole.googlecode.com/svn/trunk@287 b36e06b6-ea79-11de-95aa-47033305a456

--- greyhole.cron.d
+++ greyhole.cron.d
@@ -1,8 +1,5 @@
-# Daily fsck
-0 0 * * 1-6	root	/usr/bin/greyhole --fsck --email-report --dont-walk-graveyard > /dev/null
-
 # Weekly fsck
-0 0 * * 7	root	/usr/bin/greyhole --fsck --email-report > /dev/null
+0 0 * * 7	root	/usr/bin/greyhole --fsck --email-report --dont-walk-graveyard > /dev/null
 
 # cifs client workaround
 # Ref: http://blog.dhampir.no/content/cifs-vfs-no-response-for-cmd-n-mid
