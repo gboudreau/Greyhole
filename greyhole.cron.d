3822b802f05465585ce5571e3af937479725bb64
Guillaume Boudreau <github@danslereseau.com>
2012-10-30 20:23:59 -0300

    Modified enable_oplocks for newer Kernel module

--- greyhole.cron.d
+++ greyhole.cron.d
@@ -1,3 +1,3 @@
 # cifs client workaround
 # Ref: http://blog.dhampir.no/content/cifs-vfs-no-response-for-cmd-n-mid
-@reboot		root	/sbin/modprobe cifs && echo 0 > /proc/fs/cifs/OplockEnabled
+@reboot  	root	/sbin/modprobe cifs enable_oplocks=n; if [ -x /proc/fs/cifs/OplockEnabled ]; then echo 0 > /proc/fs/cifs/OplockEnabled; fi
