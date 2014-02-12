4b0c699ca52e5674408dca02e23de1d439c46bd3
Guillaume Boudreau <github@danslereseau.com>
2014-02-11 22:11:02 -0500

    Updated build script for VFS modules

--- build_vfs.sh
+++ build_vfs.sh
@@ -6,7 +6,7 @@
 #   - i386:   bougu@macbook:~/VirtualBox VMs/Ubuntu (32-bit)
 #   - ARM:    gb@fileserver2:~/qemu-arm
 # And don't forget to get the latest version of the samba-module from fileserver2 first:
-#   scp -r gb@192.168.155.88:Greyhole ~
+#   scp gb@192.168.155.88:Greyhole/samba-module/* ~/Greyhole/samba-module/
 
 export GREYHOLE_INSTALL_DIR="/home/gb/Greyhole"
 export HOME="/home/gb"
@@ -16,7 +16,7 @@ export HOME="/home/gb"
 cd "$HOME"
 
 cd samba-3.4.9/source3
-    if  [ ! -f bin/greyhole.so ]; then
+    if  [ ! -f modules/vfs_greyhole.c ]; then
         ./configure
         patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/Makefile-samba-3.4.patch
     fi
@@ -26,7 +26,7 @@ cd samba-3.4.9/source3
 cd ../..
 
 cd samba-3.5.4/source3
-    if  [ ! -f bin/greyhole.so ]; then
+    if  [ ! -f modules/vfs_greyhole.c ]; then
         ./configure
         patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/Makefile-samba-3.5.patch
     fi
@@ -36,7 +36,7 @@ cd samba-3.5.4/source3
 cd ../..
 
 cd samba-3.6.9/source3
-    if  [ ! -f bin/greyhole.so ]; then
+    if  [ ! -f modules/vfs_greyhole.c ]; then
         ./configure
         patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/Makefile-samba-3.6.patch
     fi
@@ -46,7 +46,7 @@ cd samba-3.6.9/source3
 cd ../..
 
 cd samba-4.0.14
-    if  [ ! -f bin/default/source3/modules/libvfs-greyhole.so ]; then
+    if  [ ! -f source3/modules/vfs_greyhole.c ]; then
         patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/wscript-samba-4.0.patch
         ./configure --enable-debug --enable-selftest --disable-symbol-versions
     fi
@@ -56,7 +56,7 @@ cd samba-4.0.14
 cd ..
 
 cd samba-4.1.4
-    if  [ ! -f bin/default/source3/modules/libvfs-greyhole.so ]; then
+    if  [ ! -f source3/modules/vfs_greyhole.c ]; then
         patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/wscript-samba-4.1.patch
         ./configure --enable-debug --enable-selftest --disable-symbol-versions
     fi
@@ -71,7 +71,7 @@ echo
 
 ARCH="`uname -i`"
 if [ "$ARCH" = "unknown" ]; then
-    ARCH="arm"
+    ARCH="armhf"
 fi
 
 if  [ -f samba-3.4.9/source3/bin/greyhole.so ]; then
@@ -118,8 +118,36 @@ echo "****************************************"
 echo
 
 exit
-SSH_HOST="gb@192.168.155.214"
+
+SSH_HOST="gb@192.168.155.206"
 ARCH="i386"
+cd ~/git/Greyhole/samba-module/bin/
+scp $SSH_HOST:Greyhole/samba-module/bin/3.4/greyhole-$ARCH.so 3.4/greyhole-$ARCH.so
+scp $SSH_HOST:Greyhole/samba-module/bin/3.5/greyhole-$ARCH.so 3.5/greyhole-$ARCH.so
+scp $SSH_HOST:Greyhole/samba-module/bin/3.6/greyhole-$ARCH.so 3.6/greyhole-$ARCH.so
+scp $SSH_HOST:Greyhole/samba-module/bin/4.0/greyhole-$ARCH.so 4.0/greyhole-$ARCH.so
+scp $SSH_HOST:Greyhole/samba-module/bin/4.1/greyhole-$ARCH.so 4.1/greyhole-$ARCH.so
+
+SSH_HOST="gb@192.168.155.88"
+ARCH="x86_64"
+cd ~/git/Greyhole/samba-module/bin/
+scp $SSH_HOST:Greyhole/samba-module/bin/3.4/greyhole-$ARCH.so 3.4/greyhole-$ARCH.so
+scp $SSH_HOST:Greyhole/samba-module/bin/3.5/greyhole-$ARCH.so 3.5/greyhole-$ARCH.so
+scp $SSH_HOST:Greyhole/samba-module/bin/3.6/greyhole-$ARCH.so 3.6/greyhole-$ARCH.so
+scp $SSH_HOST:Greyhole/samba-module/bin/4.0/greyhole-$ARCH.so 4.0/greyhole-$ARCH.so
+scp $SSH_HOST:Greyhole/samba-module/bin/4.1/greyhole-$ARCH.so 4.1/greyhole-$ARCH.so
+
+SSH_HOST="gb@127.0.0.1"
+ARCH="armhf"
+cd ~/Greyhole/samba-module/bin/
+scp -P 2223 $SSH_HOST:Greyhole/samba-module/bin/3.4/greyhole-$ARCH.so 3.4/greyhole-$ARCH.so
+scp -P 2223 $SSH_HOST:Greyhole/samba-module/bin/3.5/greyhole-$ARCH.so 3.5/greyhole-$ARCH.so
+scp -P 2223 $SSH_HOST:Greyhole/samba-module/bin/3.6/greyhole-$ARCH.so 3.6/greyhole-$ARCH.so
+scp -P 2223 $SSH_HOST:Greyhole/samba-module/bin/4.0/greyhole-$ARCH.so 4.0/greyhole-$ARCH.so
+scp -P 2223 $SSH_HOST:Greyhole/samba-module/bin/4.1/greyhole-$ARCH.so 4.1/greyhole-$ARCH.so
+
+SSH_HOST="gb@192.168.155.88"
+ARCH="armhf"
 cd ~/git/Greyhole/samba-module/bin/
 scp $SSH_HOST:Greyhole/samba-module/bin/3.4/greyhole-$ARCH.so 3.4/greyhole-$ARCH.so
 scp $SSH_HOST:Greyhole/samba-module/bin/3.5/greyhole-$ARCH.so 3.5/greyhole-$ARCH.so
