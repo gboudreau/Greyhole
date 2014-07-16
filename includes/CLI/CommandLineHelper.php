e041de63e2f4bdb30f4e5d92111c233c41b47d18
Guillaume Boudreau <github@danslereseau.com>
2014-07-15 23:22:36 -0400

    Improvement: greatly simplified command line arguments processing

--- includes/CLI/CommandLineHelper.php
+++ includes/CLI/CommandLineHelper.php
@@ -202,12 +202,12 @@ class CommandLineHelper {
     }
     
     private function getOpts() {
-        $opts = '';
+        $opts = array();
         foreach ($this->cliCommandsDefinitions as $def) {
-            $opts .= $def->getOpt();
+            $opts[] = $def->getOpt();
         }
         foreach ($this->cliOptionsDefinitions as $def) {
-            $opts .= $def->getOpt();
+            $opts[] = $def->getOpt();
         }
         return $opts;
     }
@@ -223,373 +223,134 @@ class CommandLineHelper {
         return $long_opts;
     }
 
-    protected function getopt() {
-
-    /* _getopt(): Ver. 1.3      2009/05/30
-       My page: http://www.ntu.beautifulworldco.com/weblog/?p=526
-
-    Usage: _getopt ( [$flag,] $short_option [, $long_option] );
-
-    Note that another function split_para() is required, which can be found in the same
-    page.
-
-    _getopt() fully simulates getopt() which is described at
-    http://us.php.net/manual/en/function.getopt.php , including long options for PHP
-    version under 5.3.0. (Prior to 5.3.0, long options was only available on few systems)
-
-    Besides legacy usage of getopt(), I also added a new option to manipulate your own
-    argument lists instead of those from command lines. This new option can be a string
-    or an array such as 
-
-    $flag = "-f value_f -ab --required 9 --optional=PK --option -v test -k";
-    or
-    $flag = array ( "-f", "value_f", "-ab", "--required", "9", "--optional=PK", "--option" );
-
-    So there are four ways to work with _getopt(),
-
-    1. _getopt ( $short_option );
-
-      it's a legacy usage, same as getopt ( $short_option ).
-
-    2. _getopt ( $short_option, $long_option );
-
-      it's a legacy usage, same as getopt ( $short_option, $long_option ).
-
-    3. _getopt ( $flag, $short_option );
-
-      use your own argument lists instead of command line arguments.
-
-    4. _getopt ( $flag, $short_option, $long_option );
-
-      use your own argument lists instead of command line arguments.
-
-    */
-
-      if ( func_num_args() == 1 ) {
-         $flag =  $flag_array = $GLOBALS['argv'];
-         $short_option = func_get_arg ( 0 );
-         $long_option = array ();
-         return getopt($short_option);
-      } else if ( func_num_args() == 2 ) {
-         if ( is_array ( func_get_arg ( 1 ) ) ) {
-            $flag = $GLOBALS['argv'];
-            $short_option = func_get_arg ( 0 );
-            $long_option = func_get_arg ( 1 );
-            // if (PHP_VERSION_ID >= 50300) { return getopt($short_option, $long_option); }
-         } else {
-            $flag = func_get_arg ( 0 );
-            $short_option = func_get_arg ( 1 );
-            $long_option = array ();
-            return getopt($short_option);
-         }
-      } else if ( func_num_args() == 3 ) {
-         $flag = func_get_arg ( 0 );
-         $short_option = func_get_arg ( 1 );
-         $long_option = func_get_arg ( 2 );
-         // if (PHP_VERSION_ID >= 50300) { return getopt($short_option, $long_option); }
-      } else {
-         exit ( "wrong options\n" );
-      }
-
-      $short_option = trim ( $short_option );
-
-      $short_no_value = array();
-      $short_required_value = array();
-      $short_optional_value = array();
-      $long_no_value = array();
-      $long_required_value = array();
-      $long_optional_value = array();
-      $options = array();
-
-      for ( $i = 0; $i < strlen ( $short_option ); ) {
-         if ( $short_option{$i} != ":" ) {
-            if ( $i == strlen ( $short_option ) - 1 ) {
-              $short_no_value[] = $short_option{$i};
-              break;
-            } else if ( $short_option{$i+1} != ":" ) {
-              $short_no_value[] = $short_option{$i};
-              $i++;
-              continue;
-            } else if ( $short_option{$i+1} == ":" && @$short_option{$i+2} != ":" ) {
-              $short_required_value[] = $short_option{$i};
-              $i += 2;
-              continue;
-            } else if ( $short_option{$i+1} == ":" && $short_option{$i+2} == ":" ) {
-              $short_optional_value[] = $short_option{$i};
-              $i += 3;
-              continue;
-            }
-         } else {
-            continue;
-         }
-      }
-
-      foreach ( $long_option as $a ) {
-         if ( substr( $a, -2 ) == "::" ) {
-            $long_optional_value[] = substr( $a, 0, -2);
-            continue;
-         } else if ( substr( $a, -1 ) == ":" ) {
-            $long_required_value[] = substr( $a, 0, -1 );
-            continue;
-         } else {
-            $long_no_value[] = $a;
-            continue;
-         }
-      }
-
-      if ( is_array ( $flag ) )
-         $flag_array = $flag;
-      else {
-         $flag = "- $flag";
-         $flag_array = $this->split_para( $flag );
-      }
-
-      for ( $i = 0; $i < count( $flag_array ); ) {
-
-         if ( $i >= count ( $flag_array ) )
-            break;
-
-         if ( ! $flag_array[$i] || $flag_array[$i] == "-" ) {
-            $i++;
-            continue;
-         }
-
-         if ( $flag_array[$i]{0} != "-" ) {
-            $i++;
-            continue;
-
-         }
-
-         if ( substr( $flag_array[$i], 0, 2 ) == "--" ) {
-
-            if (strpos($flag_array[$i], '=') != false) {
-              list($key, $value) = explode('=', substr($flag_array[$i], 2), 2);
-              if ( in_array ( $key, $long_required_value ) || in_array ( $key, $long_optional_value ) )
-                 $options[$key][] = $value;
-              $i++;
-              continue;
-            }
-
-            if (strpos($flag_array[$i], '=') == false) {
-              $key = substr( $flag_array[$i], 2 );
-              if ( in_array( substr( $flag_array[$i], 2 ), $long_required_value ) ) {
-                 $options[$key][] = $flag_array[$i+1];
-                 $i += 2;
-                 continue;
-              } else if ( in_array( substr( $flag_array[$i], 2 ), $long_optional_value ) ) {
-                 if ( $flag_array[$i+1] != "" && $flag_array[$i+1]{0} != "-" ) {
-                    $options[$key][] = $flag_array[$i+1];
-                    $i += 2;
-                 } else {
-                    $options[$key][] = FALSE;
-                    $i ++;
-                 }
-                 continue;
-              } else if ( in_array( substr( $flag_array[$i], 2 ), $long_no_value ) ) {
-                 $options[$key][] = FALSE;
-                 $i++;
-                 continue;
-              } else {
-                 $i++;
-                 continue;
-              }
-            }
-
-         } else if ( $flag_array[$i]{0} == "-" && $flag_array[$i]{1} != "-" ) {
-
-            for ( $j=1; $j < strlen($flag_array[$i]); $j++ ) {
-              if ( in_array( $flag_array[$i]{$j}, $short_required_value ) || in_array( $flag_array[$i]{$j}, $short_optional_value )) {
-
-                 if ( $j == strlen($flag_array[$i]) - 1  ) {
-                    if ( in_array( $flag_array[$i]{$j}, $short_required_value ) ) {
-                      $options[$flag_array[$i]{$j}][] = $flag_array[$i+1];
-                      $i += 2;
-                    } else if ( in_array( $flag_array[$i]{$j}, $short_optional_value ) && $flag_array[$i+1] != "" && $flag_array[$i+1]{0} != "-" ) {
-                      $options[$flag_array[$i]{$j}][] = $flag_array[$i+1];
-                      $i += 2;
-                    } else {
-                      $options[$flag_array[$i]{$j}][] = FALSE;
-                      $i ++;
-                    }
-                    $plus_i = 0;
-                    break;
-                 } else {
-                    $options[$flag_array[$i]{$j}][] = substr ( $flag_array[$i], $j + 1 );
-                    $i ++;
-                    $plus_i = 0;
-                    break;
-                 }
-
-              } else if ( in_array ( $flag_array[$i]{$j}, $short_no_value ) ) {
-
-                 $options[$flag_array[$i]{$j}][] = FALSE;
-                 $plus_i = 1;
-                 continue;
-
-              } else {
-                 $plus_i = 1;
-                 break;
-              }
-            }
-
-            $i += $plus_i;
-            continue;
-
-         }
-
-         $i++;
-         continue;
-      }
-
-      foreach ( $options as $key => $value ) {
-         if ( count ( $value ) == 1 ) {
-            $options[ $key ] = $value[0];
-
-         }
-
-      }
-
-      return $options;
-
-    }
-
-    protected function split_para ( $pattern ) {
-
-    /* split_para() version 1.0      2008/08/19
-       My page: http://www.ntu.beautifulworldco.com/weblog/?p=526
-
-    This function is to parse parameters and split them into smaller pieces.
-    preg_split() does similar thing but in our function, besides "space", we
-    also take the three symbols " (double quote), '(single quote),
-    and \ (backslash) into consideration because things in a pair of " or '
-    should be grouped together.
-
-    As an example, this parameter list
-
-    -f "test 2" -ab --required "t\"est 1" --optional="te'st 3" --option -v 'test 4'
-
-    will be splited into
-
-    -f
-    t"est 2
-    -ab
-    --required
-    test 1
-    --optional=te'st 3
-    --option
-    -v
-    test 4
-
-    see the code below,
-
-    $pattern = "-f \"test 2\" -ab --required \"t\\\"est 1\" --optional=\"te'st 3\" --option -v 'test 4'";
-
-    $result = split_para( $pattern );
-
-    echo "ORIGINAL PATTERN: $pattern\n\n";
-
-    var_dump( $result );
-
-    */
-
-      $begin=0;
-      $backslash = 0;
-      $quote = "";
-      $cand1 = "";
-      $quote_mark = array();
-      $result = array();
-
-      $pattern = trim ( $pattern );
-
-      for ( $end = 0; $end < strlen ( $pattern ) ; ) {
-
-         if ( ! in_array ( $pattern{$end}, array ( " ", "\"", "'", "\\" ) ) ) {
-            $backslash = 0;
-            $end ++;
-            continue;
-         }
-
-         if ( $pattern{$end} == "\\" ) {
-            $backslash++;
-            $end ++;
-            continue;
-         } else if ( $pattern{$end} == "\"" ) {
-            if ( $backslash % 2 == 1 || $quote == "'" ) {
-              $backslash = 0;
-              $end ++;
-              continue;
-            }
-
-            if ( $quote == "" ) {
-              $quote_mark[] = $end - $begin;
-              $quote = "\"";
-            } else if ( $quote == "\"" ) {
-              $quote_mark[] = $end - $begin;
-              $quote = "";
-            }
-
-            $backslash = 0;
-            $end ++;
-            continue;
-         } else if ( $pattern{$end} == "'" ) {
-            if ( $backslash % 2 == 1 || $quote == "\"" ) {
-              $backslash = 0;
-              $end ++;
-              continue;
-            }
-
-            if ( $quote == "" ) {
-              $quote_mark[] = $end - $begin;
-              $quote = "'";
-            } else if ( $quote == "'" ) {
-              $quote_mark[] = $end - $begin;
-              $quote = "";
-            }
-
-            $backslash = 0;
-            $end ++;
-            continue;
-         } else if ( $pattern{$end} == " " ) {
-            if ( $quote != "" ) {
-              $backslash = 0;
-              $end ++;
-              continue;
+    /*
+     * _getopt(): Ver. 1.3      2009/05/30
+     * My page: http://www.ntu.beautifulworldco.com/weblog/?p=526
+     *
+     * Note that another function split_para() is required, which can be found in the same page.
+     *
+     * _getopt() fully simulates getopt() which is described at
+     * http://us.php.net/manual/en/function.getopt.php , including long options for PHP
+     * version under 5.3.0. (Prior to 5.3.0, long options was only available on few systems)
+     */
+    protected function getopt($short_options, $long_options) {
+        $opts_no_value = array();
+        $opts_required_value = array();
+        $opts_optional_value = array();
+
+        foreach (array_merge($short_options, $long_options) as $a) {
+            if (substr($a, -2) == "::" ) {
+                $opts_optional_value[] = substr($a, 0, -2);
+            } else if (substr($a, -1) == ":") {
+                $opts_required_value[] = substr($a, 0, -1);
             } else {
-              $backslash = 0;
-              $cand = substr( $pattern, $begin, $end-$begin );
-              for ( $j = 0; $j < strlen ( $cand ); $j ++ ) {
-                 if ( in_array ( $j, $quote_mark ) )
+                $opts_no_value[] = $a;
+            }
+        }
+
+        $argv = $GLOBALS['argv'];
+
+        $options = array();
+        for ($i = 0; $i < count($argv); ) {
+            $arg = $argv[$i];
+            if ($arg == "-") {
+                $i++;
+                continue;
+            }
+            if ($arg{0} != "-") {
+                $i++;
+                continue;
+            }
+
+            if (!empty($argv[$i+1]) && $argv[$i+1]{0} != "-") {
+                $nextArg = $argv[$i+1];
+            } else {
+                $nextArg = FALSE;
+            }
+
+            if (substr($arg, 0, 2) == "--") {
+                // Long opt
+
+                $key = substr($arg, 2);
+
+                if (string_contains($key, '=')) {
+                    // --longopt=value
+                    list($key, $value) = explode('=', $key, 2);
+                    if (array_contains($opts_required_value, $key) || array_contains($opts_optional_value, $key)) {
+                        $options[$key][] = $value;
+                    }
+                    $i++;
                     continue;
+                }
 
-                 $cand1 .= $cand{$j};
-              }
-              if ( $cand1 ) {
-                 eval( "\$cand1 = \"$cand1\";" );
-                 $result[] = $cand1;
-              }
-              $quote_mark = array();
-              $cand1 = "";
-              $end ++;
-              $begin = $end;
-              continue;
-           }
-         }
-      }
+                if (array_contains($opts_required_value, $key)) {
+                    // --longopt value
+                    $options[$key][] = $argv[$i+1];
+                    $i += 2;
+                    continue;
+                } else if (array_contains($opts_optional_value, $key)) {
+                    // --longopt [value]
+                    if ($nextArg) {
+                        $options[$key][] = $nextArg;
+                        $i += 2;
+                    } else {
+                        $options[$key][] = FALSE;
+                        $i++;
+                    }
+                    continue;
+                } else if (array_contains($opts_no_value, $key)) {
+                    // --longopt
+                    $options[$key][] = FALSE;
+                    $i++;
+                    continue;
+                } else {
+                    $i++;
+                    continue;
+                }
+            } else {
+                // Short opt(s)
+                // eg. -abcvalue => -a -b -c value
 
-      $cand = substr( $pattern, $begin, $end-$begin );
-      for ( $j = 0; $j < strlen ( $cand ); $j ++ ) {
-         if ( in_array ( $j, $quote_mark ) )
-            continue;
+                for ($j=1; $j < strlen($arg); $j++) {
 
-         $cand1 .= $cand{$j};
-      }
+                    if (array_contains($opts_no_value, $arg{$j})) {
+                        // -a
+                        $options[$arg{$j}][] = FALSE;
+                        if ($j == strlen($arg) - 1) {
+                            break;
+                        }
+                    } if (array_contains($opts_required_value, $arg{$j})) {
+                        if ($j == strlen($arg) - 1) {
+                            // -a value
+                            $options[$arg{$j}][] = $argv[$i+1];
+                            $i++;
+                        } else {
+                            // -avalue
+                            $options[$arg{$j}][] = substr($arg, $j+1);
+                        }
+                        break;
+                    } else {
+                        // -a [value]
+                        if ($j == strlen($arg) - 1 && $nextArg) {
+                            $options[$arg{$j}][] = $nextArg;
+                            $i++;
+                        } else {
+                            $options[$arg{$j}][] = FALSE;
+                        }
+                    }
+                }
+            }
 
-      eval( "\$cand1 = \"$cand1\";" );
+            $i++;
+        }
 
-      if ( $cand1 )
-         $result[] = $cand1;
+        foreach ($options as $key => $value) {
+            if (count($value) == 1) {
+                $options[$key] = $value[0];
+            }
+        }
 
-      return $result;
+        return $options;
     }
 }
 
