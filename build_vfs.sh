810a81cc79f06ff4d5547469f0178b6bee2f0870
Guillaume Boudreau <guillaume@greyhole.net>
2016-09-17 07:02:57 -0400

    Updated VFS build script

--- build_vfs.sh
+++ build_vfs.sh
@@ -8,6 +8,7 @@
 
 # rsync -av --exclude .git --exclude release gb@192.168.155.88:Greyhole/ ~/Greyhole
 # screen
+# ~/Greyhole/build_vfs.sh
 
 export GREYHOLE_INSTALL_DIR="/home/gb/Greyhole"
 export HOME="/home/gb"
@@ -40,36 +41,36 @@ for version in 4.5.0 4.4.0 4.3.0 4.2.0 4.1.4 4.0.14 3.6.9 3.5.4 3.4.9; do
 	    rm -f source3/modules/vfs_greyhole.c source3/bin/greyhole.so bin/default/source3/modules/libvfs*greyhole.so
 	    ln -s ${GREYHOLE_INSTALL_DIR}/samba-module/vfs_greyhole-samba-${M}.x.c source3/modules/vfs_greyhole.c
 
-		if [ $M -eq 3 ]; then
+		if [ ${M} -eq 3 ]; then
 			cd source3
 		fi
 
 		if [ ! -x ${NEEDS_CONFIGURE} ]; then
-			if [ $M -eq 3 ]; then
+			if [ ${M} -eq 3 ]; then
 	            ./configure
-	            patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/Makefile-samba-$M.$m.patch
+	            patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/Makefile-samba-${M}.${m}.patch
 		    else
-		        patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/wscript-samba-$M.$m.patch
+		        patch -p1 < ${GREYHOLE_INSTALL_DIR}/samba-module/wscript-samba-${M}.${m}.patch
 		        ./configure --enable-debug --enable-selftest --disable-symbol-versions --without-acl-support --without-ldap --without-ads
 			fi
 		fi
 
 	    make -j
 
-		if [ $M -eq 3 ]; then
+		if [ ${M} -eq 3 ]; then
 			cd ..
 		fi
 
-		if [ $M -eq 3 ]; then
+		if [ ${M} -eq 3 ]; then
 			COMPILED_MODULE="source3/bin/greyhole.so"
 	    else
 	        COMPILED_MODULE="`ls -1 bin/default/source3/modules/libvfs*greyhole.so`"
 		fi
 	    ls -1 ${COMPILED_MODULE}
-	    mkdir -p ${GREYHOLE_INSTALL_DIR}/samba-module/bin/$M.$m/
-	    cp ${COMPILED_MODULE} ${GREYHOLE_INSTALL_DIR}/samba-module/bin/$M.$m/greyhole-$ARCH.so
+	    mkdir -p ${GREYHOLE_INSTALL_DIR}/samba-module/bin/${M}.${m}/
+	    cp ${COMPILED_MODULE} ${GREYHOLE_INSTALL_DIR}/samba-module/bin/${M}.${m}/greyhole-$ARCH.so
 	    echo " was copied to "
-	    ls -1 ${GREYHOLE_INSTALL_DIR}/samba-module/bin/$M.$m/greyhole-$ARCH.so
+	    ls -1 ${GREYHOLE_INSTALL_DIR}/samba-module/bin/${M}.${m}/greyhole-$ARCH.so
 
 	cd ..
 	echo
