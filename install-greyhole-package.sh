ad57d16a6ecd7dba16247909e9659299d3ad5eb0
Guillaume Boudreau <guillaume@greyhole.net>
2020-04-05 09:29:40 -0400

    deb repo: use generic labels for component name of previous versions

--- install-greyhole-package.sh
+++ install-greyhole-package.sh
@@ -139,7 +139,7 @@ elif [ "$_OSTYPE" = "apt-get" ]; then
     if apt-cache showpkg php-mbstring >/dev/null; then
         apt-get -y install php-mbstring
     fi
-    echo "deb https://www.greyhole.net/releases/deb stable main v0.12 v0.11" > /etc/apt/sources.list.d/greyhole.list
+    echo "deb https://www.greyhole.net/releases/deb stable main previous before-previous" > /etc/apt/sources.list.d/greyhole.list
     curl -s https://www.greyhole.net/releases/deb/greyhole-debsig.asc | apt-key add -
     apt-get update
     if ! apt-get -y -o DPkg::options::=--force-confmiss install greyhole; then
