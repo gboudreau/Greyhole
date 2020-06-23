8e4ba360d32a4608da227a78c014234bf6ec67f8
Guillaume Boudreau <guillaume@greyhole.net>
2020-06-23 09:45:56 -0400

    Added some steps and notes in INSTALL file, to help users manually install Greyhole

--- greyhole.cron.d
+++ greyhole.cron.d
@@ -1,6 +1,10 @@
 # cifs client workaround
 # Ref: http://blog.dhampir.no/content/cifs-vfs-no-response-for-cmd-n-mid
 @reboot    root  /sbin/modprobe cifs enable_oplocks=n; if [ -x /proc/fs/cifs/OplockEnabled ]; then echo 0 > /proc/fs/cifs/OplockEnabled; fi
+
+# On-boot initialization: create mem spool folder, mark all dangling tasks as completed, etc.
 @reboot    root  /usr/bin/greyhole --create-mem-spool >/dev/null
 @reboot    root  /usr/bin/greyhole --boot-init
+
+# Process the Samba spool as soon as possible (daemon might be busy)
 * * * * *  root  /usr/bin/greyhole --process-spool --keepalive >/dev/null
