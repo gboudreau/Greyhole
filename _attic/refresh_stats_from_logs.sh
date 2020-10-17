b7fa67e93b829e70651e8d6de9d836ed6faa193b
Guillaume Boudreau <github@danslereseau.com>
2020-10-17 12:15:22 -0400

    Update homepage

--- _attic/refresh_stats_from_logs.sh
+++ _attic/refresh_stats_from_logs.sh
@@ -1,13 +1,7 @@
-cd /usr/local/lsws/logs/
 
-apt_users=`find . -name 'www.greyhole.net*' -mtime -30 -exec gunzip -c {} \; 2>/dev/null | grep -h '/releases/deb/dists/stable/Release.gpg' | grep -vi bot | grep -vi spider | awk '{print $1}'`
-rpm_users=`find . -name 'www.greyhole.net*' -mtime -30 -exec gunzip -c {} \; 2>/dev/null | grep -h 'repodata/repomd.xml' | grep -vi bot | grep -vi spider | awk '{print $1}'`
-uniq_users=`echo "$apt_users
-$rpm_users" | sort -u | wc -l`
-echo $uniq_users > /var/www/html/greyhole.net/uniq_users.data
+cd /home/gb/nginx-logs || exit
 
-deb_downloads=`find . -name 'www.greyhole.net*' -mtime -30 -exec gunzip -c {} \; 2>/dev/null | grep -h '\.deb' | grep APT-HTTP | awk '{print $1}'`
-rpm_downloads=`find . -name 'www.greyhole.net*' -mtime -30 -exec gunzip -c {} \; 2>/dev/null | grep -h '\.rpm' | grep -vi bot | grep -vi spider | grep -v src.rpm | awk '{print $1}'`
-uniq_downloads=`echo "$deb_downloads
-$rpm_downloads" | sort -u | wc -l`
-echo $uniq_downloads > /var/www/html/greyhole.net/uniq_downloads.data
+apt_users=`find . -name 'www.greyhole.net-access.log*.gz' -exec gunzip -c {} \; 2>/dev/null | grep -h '/releases/deb/dists/stable/Release.gpg' | grep -vi bot | grep -vi spider | awk '{print $1}' ; find . -name 'www.greyhole.net-access.log*' -a -not -name 'www.greyhole.net-access.log*.gz' -exec cat {} \; 2>/dev/null | grep -h '/releases/deb/dists/stable/Release.gpg' | grep -vi bot | grep -vi spider | awk '{print $1}'`
+rpm_users=`find . -name 'www.greyhole.net-access.log*.gz' -exec gunzip -c {} \; 2>/dev/null | grep -h 'repodata/repomd.xml' | grep -vi bot | grep -vi spider | awk '{print $1}' ; find . -name 'www.greyhole.net-access.log*' -a -not -name 'www.greyhole.net-access.log*.gz' -exec cat {} \; 2>/dev/null | grep -h 'repodata/repomd.xml' | grep -vi bot | grep -vi spider | awk '{print $1}'`
+echo "Unique APT users: `echo "$apt_users" | sort -u | wc -l`"
+echo "Unique RPM users: `echo "$rpm_users" | sort -u | wc -l`"
