b84d001694bb2920016dbd3460edb0665aead6ac
Guillaume Boudreau <guillaume@greyhole.net>
2022-01-20 14:47:22 -0500

    Scorecard suggestion: pin Docker base images for VFS builds

--- build/build_vfs_all.sh
+++ build/build_vfs_all.sh
@@ -33,7 +33,8 @@ for version in 4.14.0 4.13.0 4.12.0 4.11.0 4.10.0 4.9.0 4.8.0 4.7.0 4.6.0 4.5.0 
     echo "********"
     echo "1/3 Compiling VFS for arm64"
 
-    docker build --pull --platform linux/arm64 -t greyhole-vfs-builder:arm64 --build-arg "SAMBA_VERSION=${version}" .
+    # Docker images for IMAGE arg: https://hub.docker.com/_/ubuntu?tab=tags
+    docker build --pull --platform linux/arm64 -t greyhole-vfs-builder:arm64 --build-arg "SAMBA_VERSION=${version}" --build-arg "IMAGE=ubuntu@sha256:64162ac111b666daf1305de1888eb67a3033f62000f5ff781fe529aff8f88b09" .
     id=$(docker create greyhole-vfs-builder:arm64)
     docker cp $id:/usr/share/greyhole/vfs-build/samba-$version/greyhole-samba$M$m.so "${GREYHOLE_INSTALL_DIR}/samba-module/bin/${M}.${m}/greyhole-arm64.so"
     docker rm -v $id
@@ -45,7 +46,8 @@ for version in 4.14.0 4.13.0 4.12.0 4.11.0 4.10.0 4.9.0 4.8.0 4.7.0 4.6.0 4.5.0 
     echo "********"
     echo "2/3 Compiling VFS for x86_64"
 
-    docker build --pull --platform linux/amd64 -t greyhole-vfs-builder:amd64 --build-arg "SAMBA_VERSION=${version}" .
+    # Docker images for IMAGE arg: https://hub.docker.com/_/ubuntu?tab=tags
+    docker build --pull --platform linux/amd64 -t greyhole-vfs-builder:amd64 --build-arg "SAMBA_VERSION=${version}" --build-arg "IMAGE=ubuntu@sha256:57df66b9fc9ce2947e434b4aa02dbe16f6685e20db0c170917d4a1962a5fe6a9" .
     id=$(docker create greyhole-vfs-builder:amd64)
     docker cp $id:/usr/share/greyhole/vfs-build/samba-$version/greyhole-samba$M$m.so "${GREYHOLE_INSTALL_DIR}/samba-module/bin/${M}.${m}/greyhole-x86_64.so"
     docker rm -v $id
@@ -57,7 +59,8 @@ for version in 4.14.0 4.13.0 4.12.0 4.11.0 4.10.0 4.9.0 4.8.0 4.7.0 4.6.0 4.5.0 
     echo "********"
     echo "3/3 Compiling VFS for i386"
 
-    docker build --pull --platform linux/i386 -t greyhole-vfs-builder:i386 --build-arg "SAMBA_VERSION=${version}" --build-arg "IMAGE=i386/ubuntu:latest" .
+    # Docker images for IMAGE arg: https://hub.docker.com/r/i386/ubuntu/tags
+    docker build --pull --platform linux/i386 -t greyhole-vfs-builder:i386 --build-arg "SAMBA_VERSION=${version}" --build-arg "IMAGE=i386/ubuntu@sha256:4a2efd378fc094a2b78f4478d88d8c02a828bb3adf551b903cdfe24ac0ea852f" .
     id=$(docker create greyhole-vfs-builder:i386)
     docker cp $id:/usr/share/greyhole/vfs-build/samba-$version/greyhole-samba$M$m.so "${GREYHOLE_INSTALL_DIR}/samba-module/bin/${M}.${m}/greyhole-i386.so"
     docker rm -v $id
